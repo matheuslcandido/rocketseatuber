43683aa5bef80b80bcefd976f891c7fb89b3567a
matheuslcandido <matheus.l.candido@gmail.com>
2019-05-15 04:31:50 -0300

    adicionando o componente MapView e configurando uma localizacao nele

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import MapView from 'react-native-maps';
 
 import { View } from 'react-native';
 
-const App = () => <View />;
+const App = () => (
+    <View style={{ flex: 1 }}>
+        <MapView 
+            style={{ flex: 1 }}
+            region={{ 
+                latitude: -20.8090614,
+                longitude: -49.5093683,
+                latitudeDelta: 0.0143,
+                longitudeDelta: 0.0134,
+            }}
+            showsUserLocation
+            loadingEnabled
+        />
+    </View>
+);
 
 export default App;
