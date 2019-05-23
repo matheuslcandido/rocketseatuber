d113cdcc42f2a9c3157c11187dd9fe1db3de0e7f
matheuslcandido <matheus.l.candido@gmail.com>
2019-05-23 01:35:20 -0300

    adicionando o .editorconfig e corrigindo a indentação dos arquivos criados anteriormente

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -5,44 +5,44 @@ import { View } from 'react-native';
 import Search from './../Search';
 
 export default class Map extends Component {
-    state = {
-        region: null,
-    }
-    
-    async componentDidMount() {
-        navigator.geolocation.getCurrentPosition(
-            ({ coords: { latitude, longitude } }) => {
-                this.setState({ 
-                    region: { 
-                        latitude, 
-                        longitude, 
-                        latitudeDelta: 0.0143, 
-                        longitudeDelta: 0.0134 
-                    } 
-                });
-            }, //sucesso
-            () => {}, //erro
-            {
-                timeout: 2000,
-                enableHighAccuracy: true,
-                maximumAge: 1000,
-            }
-        )
-    }
+  state = {
+    region: null,
+  }
 
-    render() {
-        const { region } = this.state;
+  async componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      ({ coords: { latitude, longitude } }) => {
+        this.setState({
+          region: {
+            latitude,
+            longitude,
+            latitudeDelta: 0.0143,
+            longitudeDelta: 0.0134
+          }
+        });
+      }, //sucesso
+      () => {}, //erro
+      {
+        timeout: 2000,
+        enableHighAccuracy: true,
+        maximumAge: 1000,
+      }
+    )
+  }
 
-        return (
-            <View style={{ flex: 1 }}>
-                <MapView 
-                    style={{ flex: 1 }}
-                    region={ region }
-                    showsUserLocation
-                    loadingEnabled
-                />
-                <Search />
-            </View>
-        )
-    }
+  render() {
+    const { region } = this.state;
+
+    return (
+      <View style={{ flex: 1 }}>
+        <MapView
+          style={{ flex: 1 }}
+          region={ region }
+          showsUserLocation
+          loadingEnabled
+        />
+        <Search />
+    </View>
+    )
+  }
 }
